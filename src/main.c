99e6feb0e6a8d65d0c5cc983182f2869d87bfdbf
emrahO-code <e.omerovic@wustl.edu>
2025-05-28 17:26:23 -0500

    added a functional camera that moves with player

--- src/main.c
+++ src/main.c
@@ -3,15 +3,20 @@
 
 int main()
 {
+	Camera2D camera;
+	camera.offset = (Vector2) {400,300};
+	camera.rotation = 0;
+	camera.zoom = 1;
+
 	Rectangle floor;
 	floor.x = 0;
 	floor.y = 500;
-	floor.width = 1000;
+	floor.width = 10000;
 	floor.height = 100;
 
 	Rectangle player;
-	player.x = 120;
-	player.y = 120;
+	player.x = 0;
+	player.y = 400;
 	player.width = 100;
 	player.height = 100;
 
@@ -26,25 +31,25 @@ int main()
 	SetTargetFPS(60);
 	while(!WindowShouldClose()){
 		float dt = GetFrameTime();
+
 		player.x += player_velo.x*dt;
 		player.y += player_velo.y*dt;
 
 		player_velo.y += GRAVITY*dt;
 
-		if((player.y >= (GetScreenHeight() - player.height)) || (player.y <= 0)){
-			player_velo.y *= -1;
-		}
-		if((player.x >= (GetScreenWidth() - player.width)) || (player.x <= 0)){
-			player_velo.x *= -1;
-		}
 		if(CheckCollisionRecs(player,floor)){
 			player_velo.y *= -1;
 		}
 
+		camera.target = (Vector2){player.x + player.width/2, 300};
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+
+		BeginMode2D(camera);
 		DrawRectangleRec(floor, BROWN);
 		DrawRectangleRec(player, GRAY);
+		EndMode2D();
 
 		EndDrawing();
 	}
